be80ab59212136d79d316134777abd8c4e757743
Charan K S <90633234+charcode123@users.noreply.github.com>
2022-06-08 22:18:40 +0530

    commit

--- server/SMTPserver.py
+++ server/SMTPserver.py
@@ -1,19 +1,38 @@
 import socket, threading
+import os
+import json
+import time
+import user_authentication as ua
+def receive_json(conn):
+    data = conn.recv(1024)
+    data = data.decode('utf-8')
+    data = json.loads(data)
+    return data
+def receive_data(conn):
+    data = conn.recv(1024)
+    data = data.decode('utf-8')
+    return data    
 class ClientThread(threading.Thread):
     def __init__(self,clientAddress,clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
         print ("New connection added: ", clientAddress)
+        self.csocket.send(bytes("Connection estblished",'UTF-8'))
     def run(self):
-        print ("Connection from : ", clientAddress)
         msg = ''
         while True:
-            data = self.csocket.recv(2048)
-            msg = data.decode()
-            if msg=='Logout':
-              break
-            print ("from client :", msg)
-            self.csocket.send(bytes(msg,'UTF-8'))
+            data=receive_json(self.csocket)
+            x=ua.user_auth(data['username'],data['password'])
+            if x==True:
+                self.csocket.send(bytes("True","UTF-8"))
+            else:
+                self.csocket.send(bytes("False","UTF-8"))
+                continue
+            choice=receive_data(self.csocket)
+            if choice=="1":
+                to=receive_data(self.csocket)    
+                
+
         print ("Client at ", clientAddress , " disconnected...")
 LOCALHOST = "127.0.0.1"
 PORT = 12345
--- server/user_validity.py
+++ server/user_validity.py
@@ -2,8 +2,8 @@ import pymongo
 client=pymongo.MongoClient("mongodb://localhost:27017/")
 db=client.Email_DB
 collections=db.Users
-def user_auth(username,password):
-    user_data=collections.find_one({"username":username,"password":password})
+def user_validity(username):
+    user_data=collections.find_one({"username":username})
     if user_data:
         return True
     else:
