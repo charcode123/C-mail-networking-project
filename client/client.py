c4e973d7a61e006855c8eff4281ebddf5cfcbce3
Charan K S <90633234+charcode123@users.noreply.github.com>
2022-06-08 21:16:28 +0530

    first

---
+++ client/client.py
@@ -0,0 +1,65 @@
+import socket
+import json
+import time
+import os
+SERVER = "127.0.0.1"
+PORT = 12345
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER, PORT))
+x=client.recv(1024)
+print(x.decode())
+time.sleep(1)
+while True:
+    os.system('cls')
+    username=input("Username: ")
+    password=input("Password: ")
+    data={"username":username,"password":password}
+    client.send(bytes(json.dumps(data),"UTF-8"))
+    x=client.recv(1024)
+    if x.decode()=="True":
+        print("Login Successful")
+        time.sleep(1)
+        os.system('cls')
+    else:
+        print("Login Failed - Try Again")
+        time.sleep(1)
+        os.system('cls')
+        continue
+    while True:
+        print("1. Send Email")
+        print("2. View Inbox")
+        print("3. View Sent")
+        print("4. Logout")
+        choice=input("Enter your choice: ")
+        if choice=="1":
+            client.send(bytes("1","UTF-8"))
+            x=client.recv(1024)
+            print(x.decode())
+            to=input("To: ")
+            y=client.send(bytes(to,"UTF-8"))
+            if y==1:
+                print("Invalid Username")
+                continue
+            subject=input("Subject: ")
+            message=input("Message: ")
+            data={"from":username,"to":to,"subject":subject,"message":message,"time":time.time()}
+            client.send(bytes(json.dumps(data),"UTF-8"))
+            x=client.recv(1024)
+            print(x.decode())
+        elif choice=="2":
+            client.send(bytes("2","UTF-8"))
+            x=client.recv(1024)
+            print(x.decode())
+            x=client.recv(1024)
+            print(x.decode())
+        elif choice=="3":
+            client.send(bytes("3","UTF-8"))
+            x=client.recv(1024)
+            print(x.decode())
+            x=client.recv(1024)
+            print(x.decode())
+        elif choice=="4":
+            client.send(bytes("4","UTF-8"))
+            x=client.recv(1024)
+            print(x.decode())
+            break            
---
+++ server/user_authentication.py
@@ -0,0 +1,12 @@
+from http import client
+from matplotlib import collections
+import pymongo
+client=pymongo.MongoClient("mongodb://localhost:27017/")
+db=client.Email_DB
+collections=db.Users
+def user_authentication(username,password):
+    user_data=collections.find_one({"username":username,"password":password})
+    if user_data:
+        return True
+    else:
+        return False
